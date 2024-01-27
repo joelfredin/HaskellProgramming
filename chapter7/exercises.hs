f2ec11de9b4ab7347bdfc7a910b6c3d8e8962e19
Joel Fredin <joel.fredin@gmail.com>
2024-01-27 18:05:47 +0100

    adding nums-case function

--- chapter7/exercises.hs
+++ chapter7/exercises.hs
@@ -26,3 +26,9 @@ ifEvenAdd2 :: Integral a => a -> a
 ifEvenAdd2 n =
     case (even n) of True -> n + 2
                      False -> n
+
+nums x =
+    case compare x 0 of
+        LT -> -1
+        GT -> 1
+        EQ -> 0
